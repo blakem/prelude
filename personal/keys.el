1df13ba70d6af76e7a260923298604804506b8c7
Blake Mills <blake@swiftstack.com>
2019-05-06 09:33:51 -0700

    goto-line; ibuffer

--- personal/keys.el
+++ personal/keys.el
@@ -1,6 +1,8 @@
-(global-set-key [(super f)] 'projectile-find-file)
-(global-set-key "\C-k" 'biv-kill-line-or-region)
+(global-set-key (kbd "s-f") 'projectile-find-file)
+(global-set-key (kbd "C-k") 'biv-kill-line-or-region)
 (global-set-key (kbd "s-1") 'delete-other-windows)
+(global-set-key (kbd "M-g") 'goto-line)
+(global-set-key (kbd "C-j") 'ibuffer)
 
 
 
--- personal/wishlist.el
+++ personal/wishlist.el
@@ -1,1 +1,4 @@
-;; C-j to switch buffers
+;; C-j      to switch buffers
+;; C-up     go up 5 lines
+;; C-down   go down 5 lines
+;; open wishlist
